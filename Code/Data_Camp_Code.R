80520643fd4c30f1d471bfd98227c2a8680eb126
MIU Radu <30793346+RaduFlorinMIU@users.noreply.github.com>
2024-02-24 18:34:40 +0100

    Miu radu (#27)
    * Initial commit
    
    * - Cheat Sheats Added
    
    * R to Python
    
    * - Data cleaning + some plots for data exploration
    
    * - Plots will be done with ggplots
    - For projects in python use markdown in VScode
    - Years to date calculation before doing some more plots
    
    * -Made seaborn plot work in markdown, even if nor perfect
    
    * Figure out how to set fig size for plots for python chunks.
    
    * - Worked on the plots
    
    * - One more plot and feel like varialbe exploration is done
    
    * - No more Ideas about Data Exploration
    
    * - Organized the repository to make it more tidy
    
    * - Got read of Useless plots
    
    * Thinks to do after meeting
    
    * - Added continets and target to dataframe + on new plot to see relation between variables
    
    * - Investors Frame + some more plots
    
    * - Got Read of Valuation plots who were useless
    
    * - One more plot
    
    * - Baseline for R migration
    
    * - Begining Transition towards R language
    
    * - R code transition
    
    * - Beging of Creating a the new dataframe and new values
    
    * - Advancements in computing the datacamp project
    
    * - Advanced in the DataCamp project Creation but not finished yet
    
    * - Completed the Datacamp Stuff
    
    * - Version of the file with code done by other persons as well
    
    * - Unclean file after : unicorn_data = unicorn_data %>%
      distinct( .keep_all = T)
    
    * - Cleaned with nice correlations tables
    
    * - Arranged the data and began To implement the Naives Biyes Model done by Shalini
    
    * - Deleting the useless part of the code
    
    * - Created functions to calculate automaticaly the Khi2 and Fisher test and return the results in a Data Frame
    
    * - Better presentation of all correlation tests
    
    * - Began Writing the report. Radu part
    
    * - Portofolio initial commit

--- Code/Data_Camp_Code.R
+++
@@ -1,7 +0,0 @@
-install.packages("pkgcond")
-library(readxl)
-suppressPackageStartupMessages(library(pkgcond))
-
-suppress_conditions(source("librs_and_funcs.R"))
-
-unicorns <- read_excel("../DataSets/CB-Insights_Global-Unicorn-Club_2023.xlsx")
